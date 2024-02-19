b9409ea718b773b36070163d033a4eab7f61c6de
hoannc0506 <hoan.nc0506@gmail.com>
2024-02-19 16:50:27 +0700

    Update script

--- scripts/zero-shot-cifar10.py
+++ scripts/zero-shot-cifar10.py
@@ -10,20 +10,12 @@ from torchvision.datasets import CIFAR10
 print(clip.available_models())
 # exit()
 
-<<<<<<< Updated upstream
-# load model
-device = "cuda:0"
-model, preprocess = clip.load("ViT-B/32", device=device, download_root="pretrained")
-=======
 device = "cuda:3"
 # import pdb; pdb.set_trace()
 model, preprocess = clip.load("ViT-B/32", device=device)
->>>>>>> Stashed changes
 
-# load dataset
 cifar10 = CIFAR10(root="data", download=True, train=False)
 
-<<<<<<< Updated upstream
 # prepare inputs
 image, class_id = cifar10[2000]
 image = preprocess(image).unsqueeze(0).to(device)
@@ -36,15 +28,10 @@ print(text.shape)
 
 # exit()
 
+
 with torch.no_grad():
     image_features = model.encode_image(image)
     text_features = model.encode_text(text)
-=======
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
-    # import pdb; pdb.set_trace()
->>>>>>> Stashed changes
     
     logits_per_image, logit_per_text = model(image, text)
     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
